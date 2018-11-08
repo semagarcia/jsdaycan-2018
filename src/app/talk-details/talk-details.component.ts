ff738d8ca89d42cc2f95d85709ad356f8a0c7081
Garcia Garcia, Jose Manuel <Jose-Manuel.Garcia@gft.com>
2018-11-08 03:15:52 +0100

    Temporal commit for local notifs

--- src/app/talk-details/talk-details.component.ts
+++ src/app/talk-details/talk-details.component.ts
@@ -7,6 +7,9 @@ import { ImageAsset } from "tns-core-modules/image-asset";
 
 import { TalksProvider } from "./../providers/talks.service";
 
+import { LocalNotifications } from "nativescript-local-notifications";
+import { Color } from "tns-core-modules/color";
+
 @Component({
     selector: "TalkDetails",
     moduleId: module.id,
@@ -36,10 +39,38 @@ export class TalkDetailsComponent implements OnInit {
 
     ngOnInit(): void {
         // Init your component properties here.
+        LocalNotifications.hasPermission();
+        this.x();
     }
 
     goBack(): void {
         this.routerExtSrv.backToPreviousPage();
+    }
+
+    x() {
+        LocalNotifications.schedule([{
+            id: 1,
+            title: 'The title',
+            body: 'Recurs every minute until cancelled',
+            ticker: 'The ticker',
+            color: new Color("red"),
+            badge: 1,
+            groupedMessages: ["The first", "Second", "Keep going", "one more..", "OK Stop"], //android only
+            groupSummary: "Summary of the grouped messages above", //android only
+            ongoing: true, // makes the notification ongoing (Android only)
+            icon: 'res://heart',
+            image: "https://cdn-images-1.medium.com/max/1200/1*c3cQvYJrVezv_Az0CoDcbA.jpeg",
+            thumbnail: true,
+            interval: 'minute',
+            channel: 'My Channel', // default: 'Channel'
+            sound: "customsound-ios.wav", // falls back to the default sound on Android
+            at: new Date(new Date().getTime() + (10 * 1000)) // 10 seconds from now
+          }]).then(() => {
+                console.log("Notification scheduled");
+              }, (error) => {
+                console.log("scheduling error: " + error);
+              }
+          );
     }
 
     onTap($event) {
